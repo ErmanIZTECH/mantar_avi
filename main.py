2880b454ed709bad39bbddaa485cd4dab3013c75
ErmanIZTECH <ermanolcay@gmail.com>
2022-11-23 01:54:11 +0300

    1st_working implementation

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-#hello 
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+if __name__ == "__main__" :
+    # Collect first page of artists’ list
+    links = {'Yamanlar':'https://www.meteoblue.com/tr/hava/historyclimate/weatherarchive/yamanlar-da%c4%9f%c4%b1_t%c3%bcrkiye_297765?fcstlength=15&year=2022&month=11',
+            'Balcova':'https://www.meteoblue.com/tr/hava/historyclimate/weatherarchive/bal%c3%a7ova-baraj%c4%b1_t%c3%bcrkiye_9888632?fcstlength=15&year=2022&month=11',
+            'Kaynaklar':'https://www.meteoblue.com/tr/hava/historyclimate/weatherarchive/kurudere_t%c3%bcrkiye_305410?fcstlength=15&year=2022&month=11'}
+    for il in links.keys():
+        page = requests.get(links[il])
+        # Create a BeautifulSoup object
+        soup = BeautifulSoup(page.text, 'html.parser')
+
+        # Pull all text from the BodyText div
+        name_list = soup.find(id='chart_download')
+
+        # Pull text from all instances of <a> tag within BodyText div
+        print(name_list.attrs['href'])
+        response = requests.get("https:"+name_list.attrs['href'])
+        imageName = "image_"+il+".png"
+        open(imageName, "wb").write(response.content)
+        # for parent in name_list.parents:
+        #     print(parent.name)
+
+        # for link in soup.find_all('a'):
+        #     print(link.get('href'))
+
+
+
