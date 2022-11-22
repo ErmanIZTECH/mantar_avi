4e5bcaadf17eaabeee47e86d175e99fbe0a4ef87
ErmanIZTECH <ermanolcay@gmail.com>
2022-11-23 02:01:43 +0300

    made year and month automatic

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from datetime import datetime
 
 if __name__ == "__main__" :
     currentMonth = datetime.now().month
+    currentYear = datetime.now().year
     # Collect first page of artists’ list
-    links = {'Yamanlar':'https://www.meteoblue.com/tr/hava/historyclimate/weatherarchive/yamanlar-da%c4%9f%c4%b1_t%c3%bcrkiye_297765?fcstlength=15&year=2022&month='+str(currentMonth),
-            'Balcova':'https://www.meteoblue.com/tr/hava/historyclimate/weatherarchive/bal%c3%a7ova-baraj%c4%b1_t%c3%bcrkiye_9888632?fcstlength=15&year=2022&month='+str(currentMonth),
-            'Kaynaklar':'https://www.meteoblue.com/tr/hava/historyclimate/weatherarchive/kurudere_t%c3%bcrkiye_305410?fcstlength=15&year=2022&month='+str(currentMonth)}
+    links = {'Yamanlar':'https://www.meteoblue.com/tr/hava/historyclimate/weatherarchive/yamanlar-da%c4%9f%c4%b1_t%c3%bcrkiye_297765?fcstlength=15&year='+str(currentYear)+'&month='+str(currentMonth),
+            'Balcova':'https://www.meteoblue.com/tr/hava/historyclimate/weatherarchive/bal%c3%a7ova-baraj%c4%b1_t%c3%bcrkiye_9888632?fcstlength=15&year='+str(currentYear)+'&month='+str(currentMonth),
+            'Kaynaklar':'https://www.meteoblue.com/tr/hava/historyclimate/weatherarchive/kurudere_t%c3%bcrkiye_305410?fcstlength=15&year='+str(currentYear)+'&month='+str(currentMonth)}
     for il in links.keys():
         page = requests.get(links[il])
         # Create a BeautifulSoup object
