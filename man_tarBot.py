1c658a3ab59e1f5046734d54b6f9b517d36dd455
ErmanIZTECH <ermanolcay@gmail.com>
2022-11-24 02:08:22 +0300

    api used from txt.

--- man_tarBot.py
+++ man_tarBot.py
@@ -4,22 +4,21 @@ from telegram.ext.callbackcontext import CallbackContext
 from telegram.ext.commandhandler import CommandHandler
 from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
-from main import last15daysReport
+import main
 
 if __name__ == "__main__" :
-    
-
-    last15daysReport() 
-    bot_api = open('botapi.txt','rb')
-    updater = Updater(bot_api, 
+        apiFile = open('botapi.txt','r')
+        api = apiFile.read()        
+        updater = Updater(api,
                     use_context=True)
+        # main.last15daysReport() 
 
-    def start(update: Update, context: CallbackContext):
+        def start(update: Update, context: CallbackContext):
             update.message.reply_text(
             "Merhaba *man-tar* mantar avlarında kullanılmak üzere yazılmış bir telegram botudur.\n"+
             "Komuları görmek için /help komutunu kullanın.")
 
-    def help(update: Update, context: CallbackContext):
+        def help(update: Update, context: CallbackContext):
             update.message.reply_text( 
             "/start : Genel bilgilendirme mesajını gösterir.\n"+
             "/help : Komutları listeler.\n"+
@@ -27,20 +26,20 @@ if __name__ == "__main__" :
             "/balcova : balcova bölgesi 15 günlük raporu.\n"+
             "/kaynaklar : kaynaklar bölgesi 15 günlük raporu.\n")
 
-    def yamanlar(update: Update, context: CallbackContext):
+        def yamanlar(update: Update, context: CallbackContext):
             update.message.reply_photo(photo=open('image_Yamanlar.png','rb'), 
             caption = "Yamanlar bölgesi son 15 günün yağış bilgisi")
-    def balcova(update: Update, context: CallbackContext):
+        def balcova(update: Update, context: CallbackContext):
             update.message.reply_photo(photo=open('image_Balcova.png','rb'),
             caption = "Balçova bölgesi son 15 günün yağış bilgisi")
-    def kaynaklar(update: Update, context: CallbackContext):
+        def kaynaklar(update: Update, context: CallbackContext):
             update.message.reply_photo(photo=open('image_Kaynaklar.png','rb'),
             caption = "Kaynaklar bölgesi son 15 günün yağış bilgisi")
 
-    updater.dispatcher.add_handler(CommandHandler('start', start))
-    updater.dispatcher.add_handler(CommandHandler('help', help))
-    updater.dispatcher.add_handler(CommandHandler('yamanlar', yamanlar))
-    updater.dispatcher.add_handler(CommandHandler('balcova', balcova))
-    updater.dispatcher.add_handler(CommandHandler('kaynaklar', kaynaklar))
+        updater.dispatcher.add_handler(CommandHandler('start', start))
+        updater.dispatcher.add_handler(CommandHandler('help', help))
+        updater.dispatcher.add_handler(CommandHandler('yamanlar', yamanlar))
+        updater.dispatcher.add_handler(CommandHandler('balcova', balcova))
+        updater.dispatcher.add_handler(CommandHandler('kaynaklar', kaynaklar))
 
-    updater.start_polling()
+        updater.start_polling()
