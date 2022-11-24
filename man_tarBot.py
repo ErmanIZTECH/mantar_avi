7bc343fcb1eaf9082a73b74a8fcff9641ac97f06
Mavrikant <Mavrikant@users.noreply.github.com>
2022-11-24 16:40:24 +0000

    :art: Format Python code with psf/black

--- man_tarBot.py
+++ man_tarBot.py
@@ -6,40 +6,49 @@ from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
 import main
 
-if __name__ == "__main__" :
-        apiFile = open('botapi.txt','r')
-        api = apiFile.read()        
-        updater = Updater(api,
-                    use_context=True)
-        # main.last15daysReport() 
+if __name__ == "__main__":
+    apiFile = open("botapi.txt", "r")
+    api = apiFile.read()
+    updater = Updater(api, use_context=True)
+    # main.last15daysReport()
 
-        def start(update: Update, context: CallbackContext):
-            update.message.reply_text(
-            "Merhaba *man-tar* mantar avlarında kullanılmak üzere yazılmış bir telegram botudur.\n"+
-            "Komuları görmek için /help komutunu kullanın.")
+    def start(update: Update, context: CallbackContext):
+        update.message.reply_text(
+            "Merhaba *man-tar* mantar avlarında kullanılmak üzere yazılmış bir telegram botudur.\n"
+            + "Komuları görmek için /help komutunu kullanın."
+        )
 
-        def help(update: Update, context: CallbackContext):
-            update.message.reply_text( 
-            "/start : Genel bilgilendirme mesajını gösterir.\n"+
-            "/help : Komutları listeler.\n"+
-            "/yamanlar : Yamanlar bölgesi 15 günlük raporu.\n"+
-            "/balcova : balcova bölgesi 15 günlük raporu.\n"+
-            "/kaynaklar : kaynaklar bölgesi 15 günlük raporu.\n")
+    def help(update: Update, context: CallbackContext):
+        update.message.reply_text(
+            "/start : Genel bilgilendirme mesajını gösterir.\n"
+            + "/help : Komutları listeler.\n"
+            + "/yamanlar : Yamanlar bölgesi 15 günlük raporu.\n"
+            + "/balcova : balcova bölgesi 15 günlük raporu.\n"
+            + "/kaynaklar : kaynaklar bölgesi 15 günlük raporu.\n"
+        )
 
-        def yamanlar(update: Update, context: CallbackContext):
-            update.message.reply_photo(photo=open('image_Yamanlar.png','rb'), 
-            caption = "Yamanlar bölgesi son 15 günün yağış bilgisi")
-        def balcova(update: Update, context: CallbackContext):
-            update.message.reply_photo(photo=open('image_Balcova.png','rb'),
-            caption = "Balçova bölgesi son 15 günün yağış bilgisi")
-        def kaynaklar(update: Update, context: CallbackContext):
-            update.message.reply_photo(photo=open('image_Kaynaklar.png','rb'),
-            caption = "Kaynaklar bölgesi son 15 günün yağış bilgisi")
+    def yamanlar(update: Update, context: CallbackContext):
+        update.message.reply_photo(
+            photo=open("image_Yamanlar.png", "rb"),
+            caption="Yamanlar bölgesi son 15 günün yağış bilgisi",
+        )
 
-        updater.dispatcher.add_handler(CommandHandler('start', start))
-        updater.dispatcher.add_handler(CommandHandler('help', help))
-        updater.dispatcher.add_handler(CommandHandler('yamanlar', yamanlar))
-        updater.dispatcher.add_handler(CommandHandler('balcova', balcova))
-        updater.dispatcher.add_handler(CommandHandler('kaynaklar', kaynaklar))
+    def balcova(update: Update, context: CallbackContext):
+        update.message.reply_photo(
+            photo=open("image_Balcova.png", "rb"),
+            caption="Balçova bölgesi son 15 günün yağış bilgisi",
+        )
 
-        updater.start_polling()
+    def kaynaklar(update: Update, context: CallbackContext):
+        update.message.reply_photo(
+            photo=open("image_Kaynaklar.png", "rb"),
+            caption="Kaynaklar bölgesi son 15 günün yağış bilgisi",
+        )
+
+    updater.dispatcher.add_handler(CommandHandler("start", start))
+    updater.dispatcher.add_handler(CommandHandler("help", help))
+    updater.dispatcher.add_handler(CommandHandler("yamanlar", yamanlar))
+    updater.dispatcher.add_handler(CommandHandler("balcova", balcova))
+    updater.dispatcher.add_handler(CommandHandler("kaynaklar", kaynaklar))
+
+    updater.start_polling()
