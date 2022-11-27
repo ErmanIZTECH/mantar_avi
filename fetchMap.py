2d0e577cc35504194d7b540f00b5a55c783ef928
ErmanIZTECH <ErmanIZTECH@users.noreply.github.com>
2022-11-27 00:17:16 +0000

    :art: Format Python code with psf/black

--- fetchMap.py
+++ fetchMap.py
@@ -15,11 +15,18 @@ yEnd = 25205
 
 link = base + str(layer) + "/" + str(yStart) + "/" + str(xStart)
 img_1 = Image.open(BytesIO(requests.get(link).content)).convert("RGB")
-fullImage = Image.new('RGB',((xEnd-xStart+1)*img_1.size[0], (yEnd-yStart+1)*img_1.size[1]), (250,250,250))
-for x in range(xStart, xEnd+1):
-    for y in range(yStart, yEnd+1):
+fullImage = Image.new(
+    "RGB",
+    ((xEnd - xStart + 1) * img_1.size[0], (yEnd - yStart + 1) * img_1.size[1]),
+    (250, 250, 250),
+)
+for x in range(xStart, xEnd + 1):
+    for y in range(yStart, yEnd + 1):
         link = base + str(layer) + "/" + str(y) + "/" + str(x)
-        fullImage.paste(Image.open(BytesIO(requests.get(link).content)).convert("RGB"),((x-xStart)*img_1.size[0],(y-yStart)*img_1.size[1]))
+        fullImage.paste(
+            Image.open(BytesIO(requests.get(link).content)).convert("RGB"),
+            ((x - xStart) * img_1.size[0], (y - yStart) * img_1.size[1]),
+        )
         # Image.fromarray(imgData).show()
 
 fullImage.show()
