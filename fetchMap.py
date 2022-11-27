7837154f5122aaf1776a2c8b0677c66623bd0b7e
ErmanIZTECH <ermanolcay@gmail.com>
2022-11-27 03:16:40 +0300

    can fetch n by n image but performance updated rquired

--- fetchMap.py
+++ fetchMap.py
@@ -1,23 +1,25 @@
+import os
 import requests
 from PIL import Image
 from io import BytesIO
 import numpy as np
+from npy_append_array import NpyAppendArray
 
 base = "https://cbs.ogm.gov.tr/arcgis/rest/services/YAYIN/ARAZI_ORTUSU/MapServer/tile/"
 layer = 16
 xStart = 37591
-xEnd = 37592
-yStart = 25193
-yEnd = 25193
+xEnd = 37605
+yStart = 25191
+yEnd = 25205
 
-fullImg = ""
-for y in range(yStart, yEnd):
-    for x in range(xStart, xEnd):
+
+link = base + str(layer) + "/" + str(yStart) + "/" + str(xStart)
+img_1 = Image.open(BytesIO(requests.get(link).content)).convert("RGB")
+fullImage = Image.new('RGB',((xEnd-xStart+1)*img_1.size[0], (yEnd-yStart+1)*img_1.size[1]), (250,250,250))
+for x in range(xStart, xEnd+1):
+    for y in range(yStart, yEnd+1):
         link = base + str(layer) + "/" + str(y) + "/" + str(x)
-        imgData = np.array(
-            Image.open(BytesIO(requests.get(link).content)).convert("RGB")
-        )
-        fullImg = np.concatenate((fullImg, imgData), axis=0)
+        fullImage.paste(Image.open(BytesIO(requests.get(link).content)).convert("RGB"),((x-xStart)*img_1.size[0],(y-yStart)*img_1.size[1]))
+        # Image.fromarray(imgData).show()
 
-out = Image.fromarray(fullImg)
-out.show()
+fullImage.show()
